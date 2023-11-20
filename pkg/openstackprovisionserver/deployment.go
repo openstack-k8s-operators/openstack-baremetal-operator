755189a1a902ff228dae005567a3e35c21a51ddc
Harald Jensås <hjensas@redhat.com>
2023-11-20 20:21:46 +0100

    Provisioning server ready probe longe init delay
    Increase the PeriodSeconds to 10 and InitialDelaySeconds
    to 30 for the openstakcprovisioningservers readinessProbe and
    livenessProbe.
    
    I am experiencing an issue where the pod never starts because
    the probes are restarting the pod before the certificate generation
    process has completed.
    
    ```
    Normal  Killing  4m44s  kubelet  Container osp-httpd failed liveness probe, will be restarted
    ```
    
    ```
    $ oc logs -n openstack pod/openstack-edpm-ipam-provisionserver-openstackprovisionserv6vcjp
    Defaulted container "osp-httpd" out of: osp-httpd, osp-provision-ip-discovery-agent, init (init)
    => sourcing 10-set-mpm.sh ...
    => sourcing 20-copy-config.sh ...
    => sourcing 40-ssl-certs.sh ...
    ---> Generating SSL key pair for httpd...
    ```
    
    Signed-off-by: Harald Jensås <hjensas@redhat.com>

--- pkg/openstackprovisionserver/deployment.go
+++ pkg/openstackprovisionserver/deployment.go
@@ -43,13 +43,13 @@ func Deployment(
 		// TODO might need tuning
 		TimeoutSeconds:      5,
 		PeriodSeconds:       3,
-		InitialDelaySeconds: 3,
+		InitialDelaySeconds: 30,
 	}
 	readinessProbe := &corev1.Probe{
 		// TODO might need tuning
 		TimeoutSeconds:      5,
 		PeriodSeconds:       5,
-		InitialDelaySeconds: 5,
+		InitialDelaySeconds: 30,
 	}
 
 	args := []string{"-c"}
