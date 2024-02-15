b9726a934ecdbf43537d921120cb1f5cc839ac83
Brendan Shephard <bshephar@redhat.com>
2024-02-15 19:15:20 +1000

    Unique LeaderElectionID
    Signed-off-by: Brendan Shephard <bshephar@redhat.com>

--- main.go
+++ main.go
@@ -90,7 +90,7 @@ func main() {
 		},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "c8c223a1.openstack.org",
+		LeaderElectionID:       "dedc2245.openstack.org",
 		WebhookServer: webhook.NewServer(
 			webhook.Options{
 				Port:    9443,
